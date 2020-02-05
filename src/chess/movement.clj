9696fbe4d21f3a6d87b3ab2ecb76b0abef10e65a
Shubham Chauhan <shubham25may1998@gmail.com>
2020-02-05 17:19:08 +0530

    #000: Ankon | Chauhan : Added ally check in possible moves

--- src/chess/movement.clj
+++ src/chess/movement.clj
@@ -18,45 +18,61 @@
 
 (defn empty-place? [board pos] (nil? (board (twod-1d pos))))
 
+(defn ally? [piece board pos]
+  (= (:color piece) (-> pos
+                        twod-1d
+                        board
+                        :color)))
+
 ;******************************************************************
-(defmulti moves-of :type)
+(defmulti moves-of (fn [piece _] (isa? (:type piece) ::one-jump-piece)))
 
-(defmethod moves-of :king [piece displacement]
+::one-jump-piece ::continuous-jump-piece
+
+(derive ::king ::one-jump-piece)
+(derive ::knight ::one-jump-piece)
+(derive ::pawn ::one-jump-piece)
+
+(derive ::queen ::continuous-jump-piece)
+(derive ::rook ::continuous-jump-piece)
+(derive ::bishop ::continuous-jump-piece)
+
+(defmethod moves-of true [piece displacement]
   (take-while #(lies-in-range % THRESHOLD) (possible-move (:pos piece) displacement)))
 
-(defmethod moves-of :knight [piece displacement]
-  (take-while #(lies-in-range % THRESHOLD) (possible-move (:pos piece) displacement)))
-
-(defmethod moves-of :pawn [piece displacement]
-  (take-while #(lies-in-range % THRESHOLD) (possible-move (:pos piece) displacement)))
-
-(defmethod moves-of :default [piece displacement]
+(defmethod moves-of false [piece displacement]
   (rest
     (take-while #(lies-in-range % THRESHOLD) (possible-moves (:pos piece) displacement))))
 
 ;******************************************************************
 (defmulti piece-possible-moves :type)
 
-(defmethod piece-possible-moves [:queen :king] [piece]
+(defmethod piece-possible-moves ::queen [piece]
   (map (partial moves-of piece) ALL-DISPLACEMENTS))
 
-(defmethod piece-possible-moves :rook [piece]
+(defmethod piece-possible-moves ::king [piece]
+  (map (partial moves-of piece) ALL-DISPLACEMENTS))
+
+(defmethod piece-possible-moves ::rook [piece]
   (map (partial moves-of piece) ROOK-DISPLACEMENTS))
 
-(defmethod piece-possible-moves :pawn [piece]
+(defmethod piece-possible-moves ::pawn [piece]
   (let [displacements (cond
-                        (= (:color piece) :black BLACK-PAWN-DISPLACEMENTS)
+                        (= (:color piece) :black) BLACK-PAWN-DISPLACEMENTS
                         (= (:color piece) :white) WHITE-PAWN-DISPLACEMENTS)]
     (map (partial moves-of piece) displacements)))
 
-(defmethod piece-possible-moves :bishop [piece]
+(defmethod piece-possible-moves ::bishop [piece]
   (map (partial moves-of piece) BISHOP-DISPLACEMENTS))
 
-(defmethod piece-possible-moves :knight [piece]
+(defmethod piece-possible-moves ::knight [piece]
   (map (partial moves-of piece) KNIGHT-DISPLACEMENTS))
 
 ;******************************************************************
 (def create-board (vec (repeat 64 nil)))
 
-(defn filter-valid [possible-moves board]
-  (mapcat #(take-while (partial empty-place? board) %) possible-moves))
+(defn filter-valid [piece board x] (->> (take-until (partial empty-place? board) x)
+                                        (remove (partial ally? piece board))))
+
+(defn valid-moves [piece board]
+  (mapcat (partial filter-valid piece board) (piece-possible-moves piece)))
